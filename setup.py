219c442daa06e37795f91d1d82b08215008b81cc
Amey Golait <amee@Ameys-MacBook-Air.local>
2025-07-15 17:04:21 +0530

    Add setup.py for packaging and installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clinic_app',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
